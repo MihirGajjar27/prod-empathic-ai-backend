f1a73be1900c6d91b0992d9cf43ff22e6fe3dfc8
Zach Peng <zp1234000@gmail.com>
2026-02-28 12:14:04 -0800

    Add FastAPI app structure, routes, and project config

---
+++ apps/api/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+
+    API_V1_STR: str = "/api/v1"
+
+    PROJECT_NAME: str = "Empathic-AI"
+    # GEMINI_API_KEY: str
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from apps.api.core.config import settings
+from apps.api.api.main import api_router
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    docs_url=f"{settings.API_V1_STR}/docs",
+    redoc_url=f"{settings.API_V1_STR}/redoc",
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# CORS, this is important!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://localhost:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
