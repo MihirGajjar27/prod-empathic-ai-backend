52c3fbdb26f7c2d490d52a77ad478028cac1d71a
spoigai21 <spoigai21@gmail.com>
2026-02-28 15:49:02 -0800

    Add WebSocket protocol and session handler

---
+++ apps/api/ws/session_ws.py
@@ -0,0 +1,44 @@
+from typing import Any
+
+from fastapi import APIRouter, WebSocket
+
+router = APIRouter(tags=['ws'])
+
+@router.websocket("/ws/session/{session_id}")
+async def session_ws_endpoint(websocket: WebSocket, session_id: str) -> None:
+
+    pass
+
+async def receive_client_packet(websocket: WebSocket) -> dict[str,Any]:
+
+    pass
+
+async def process_client_packet(
+        *,
+        session_id: str,
+        raw_packet: dict[str,Any],
+        websocket: WebSocket,
+) -> None:
+    
+    pass
+
+async def handle_evi_user_message_event(
+        *,
+        session_id: str,
+        payload: dict[str, Any],
+        websocket: WebSocket,
+) -> None:
+    pass
+
+
+async def send_server_error(
+        *,
+        websocket: WebSocket,
+        session_id: str,
+        code: str,
+        correaltion_id: str | None = None,
+        retryable: bool = False,
+        details: dict[str,Any] | None = None,
+) -> None: 
+    pass
+
